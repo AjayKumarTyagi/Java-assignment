d415ddd8de95573ff51ccaa07c841b65c358bfdd
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-03-01 16:54:40 +0530

    Class tiger added which inherits Animal class

--- AnimalZoo.java
+++ AnimalZoo.java
@@ -8,3 +8,23 @@ abstract class Animal
   abstract public int getAge(int age);
   abstract public void talk();
 }
+
+class Tiger extends Animal
+{
+  public String getName(String name)
+  {
+    this.type="tiger";
+    this.name=name;
+    return this.name;
+  }
+  public int getAge(int age)
+  {
+    this.age=age;
+    return this.age;
+  }
+  public void talk()
+  {
+    System.out.println("Tiger roars, and says ");
+    System.out.println("Hello, my name is: "+this.name+" I am "+this.age+" years old, I want to eat you");
+  }
+}
