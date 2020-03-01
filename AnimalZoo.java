e426bf99b8d4c86d48f50d899406dac63d023f8f
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-03-01 16:53:55 +0530

    abstract class Animal created

---
+++ AnimalZoo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+abstract class Animal
+{
+  String name;
+  int age;
+  String type;
+  abstract public String getName(String name);
+  abstract public int getAge(int age);
+  abstract public void talk();
+}
