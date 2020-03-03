2607ecb42c4ae2449eaf337ec7188fd9ba409075
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-03-03 23:42:40 +0530

    Publication class added

--- BookStore.java
+++ BookStore.java
@@ -4,3 +4,24 @@ interface Sales
   public void getSales(int sales);
   public void DisplaySales();
 }
+
+class Publication implements Sales
+{
+  int sales;
+  String title;
+  double price;
+  Publication(String title, double price)
+  {
+    this.title=title;
+    this.price=price;
+    System.out.println("Publication super class constructor called");
+  }
+  public void getSales(int sales)
+  {
+    this.sales=sales;
+  }
+  public void DisplaySales()
+  {
+    System.out.println("Total sales: "+this.sales);
+  }
+}
