9507531b623f8f1e32d0330fe88fa0fc8a947806
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-03-09 00:05:18 +0530

    Global variables declared

--- Bank.java
+++ Bank.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+class Account
+{
+  String customer_name;
+  static int accno=19216800;
+  int accountno;
+  String typeofacc;
+  double balance;
+  final double ser_charge=0.00314;
+  final double minbalance=5000.0;
+  String type="";
+  double principle;
+  double rate=6.0;
